f5343d3ef233054eaf6b5ac22951298c75fa5361
nlhchinh <chinhnguyen1692000z@gmail.com>
2025-11-09 01:34:27 +0700

    Change merchant account, new tmdcode, hash code

--- src/OrderService/OrderService.Infrastructure/Helper/VnPayConfig.cs
+++ src/OrderService/OrderService.Infrastructure/Helper/VnPayConfig.cs
@@ -3,8 +3,8 @@ namespace OrderService.Application.Services.Payment
     public class VNPayConfig
     {
         // Lấy từ tài liệu (hoặc đăng ký theo link VNPAY cung cấp)
-        public string TmnCode { get; set; } = "2AN3Q8IC"; 
-        public string HashSecret { get; set; } = "BYKRZELHX3EZKBIDZDAEN3XLHTJ51728";
+        public string TmnCode { get; set; } = "7K0SN0PD"; 
+        public string HashSecret { get; set; } = "VVIY4BH2WA5J9H9ZHAOLKN3DR2LD6EWX";
         public string BaseUrl { get; set; } = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
         
         // URL Front-end sau khi thanh toán xong
